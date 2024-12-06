1c10b0e987e303dec58b9986130d3be808dde49a
Eunyeong Park <eunyeong.cloud@gmail.com>
2024-12-06 16:50:06 +0900

    1_to_tanstack 완료

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,24 +1,39 @@
 import { useState } from "react";
 import { todoApi } from "../api/todos";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-export default function TodoForm({ fetchData }) {
+export default function TodoForm() {
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
 
   // TODO: 필수: useMutation 으로 리팩터링 하세요.
   // TODO: 선택: useMutation 으로 리팩터링 후, useTodoMutation 커스텀훅으로 정리해 보세요.
-  const handleAddTodo = async (e) => {
+
+  const queryClient = useQueryClient();
+
+  const { mutate: mutatePostTodo } = useMutation({
+    mutationFn: async () => {
+      await todoApi.post("/todos", {
+        id: Date.now().toString(),
+        title,
+        contents,
+        isCompleted: false,
+        createdAt: Date.now(),
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ["todos"]});
+      setTitle("");
+      setContents("");
+    },
+    onError: (error) => {
+      console.log('error 발생', error);
+    }
+   });
+
+  const handleAddTodo = (e) => {
     e.preventDefault();
-    setTitle("");
-    setContents("");
-    await todoApi.post("/todos", {
-      id: Date.now().toString(),
-      title,
-      contents,
-      isCompleted: false,
-      createdAt: Date.now(),
-    });
-    await fetchData();
+    mutatePostTodo();
   };
 
   return (
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,6 @@
-import { useEffect } from "react";
-import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { todoApi } from "../api/todos";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Detail() {
   const { id } = useParams();
@@ -10,26 +9,15 @@ export default function Detail() {
   // TODO: 필수: useQuery 로 리팩터링 하세요.
   // TODO: 선택: useQuery 로 리팩터링 후, useTodoQuery 커스텀훅으로 정리해 보세요.
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
+  const { data: todos, isPending, error} = useQuery({
+    queryKey: ["todos", id],
+    queryFn: async () => {
+      const response = await todoApi(`/todos/${id}`);
+      return response.data;
+    }
+  });
 
-  useEffect(() => {
-    const fetchDetail = async () => {
-      try {
-        const response = await todoApi(`/todos/${id}`);
-        setData(response.data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchDetail();
-  }, [id]);
-
-  if (isLoading) return <div style={{ fontSize: 36 }}>로딩중...</div>;
+  if (isPending) return <div style={{ fontSize: 36 }}>로딩중...</div>;
   if (error) {
     console.error(error);
     return (
@@ -40,9 +28,9 @@ export default function Detail() {
   return (
     <div>
       <button onClick={() => navigate("/")}>홈으로 이동</button>
-      <p>제목: {data.title}</p>
-      <p>내용: {data.contents}</p>
-      <p>작성일자: {new Date(data.createdAt).toDateString()}</p>
+      <p>제목: {todos.title}</p>
+      <p>내용: {todos.contents}</p>
+      <p>작성일자: {new Date(todos.createdAt).toDateString()}</p>
     </div>
   );
 }
